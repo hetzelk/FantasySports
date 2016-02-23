dbc8588f0b143292a6f1b3199e3de338411e6f9d
keithhetzel <keithh.8112@yahoo.com>
2016-02-23 15:06:10 -0600

    adding initial files

---
+++ RotoSports/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RotoSports.MvcApplication" Language="C#" %>
---
+++ RotoSports/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RotoSports.Startup))]
+namespace RotoSports
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
